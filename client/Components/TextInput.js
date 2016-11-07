db032a23e92568b1787e88abf65b4d5e59d85266
Peter Albertson <albertson.peter@gmail.com>
2016-11-07 15:31:55 -0800

    initial creation of text input component

---
+++ client/Components/TextInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+
+class TextInput extends React.Component {
+
+
+  render () {
+    const textInputClasses = [
+      "TextInput",
+      "mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
+    ];
+
+    return (
+      <div className={classNames(textInputClasses)} >
+        <input id={this.props.inputName.replace(' ', '-').toLowerCase()} className="mdl-textfield__input" type="text" name={this.props.inputName}  ref={this.props.inputRef} />
+        <label htmlFor={this.props.inputName.replace(' ', '-').toLowerCase()} className="mdl-textfield__label" >{this.props.inputName}</label>
+      </div>
+    );
+  }
+
+}
+
+export default TextInput;
