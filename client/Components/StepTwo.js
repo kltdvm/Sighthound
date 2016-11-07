8ed5971904947337446bb90ad9eb240fae9f8435
Peter Albertson <albertson.peter@gmail.com>
2016-11-07 15:33:44 -0800

    initial stab at creating patient info step

--- client/Components/StepTwo.js
+++ client/Components/StepTwo.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import classNames from 'classnames';
+import Section from './Section';
+import SectionHeader from './SectionHeader';
+import TextInput from './TextInput';
+import NumberInput from './NumberInput';
+import Dropdown from './Dropdown';
+import NextStep from './NextStep';
 
 class StepTwo extends React.Component {
 
@@ -12,9 +18,33 @@ class StepTwo extends React.Component {
       this.props.currentStep === step ? "is-step-2" : null
     ];
 
+    const sexOptions = [
+      {
+        id: 0,
+        name: "Sex"
+      },
+      {
+        id: 1,
+        name: "Male",
+        value: "male"
+      },
+      {
+        id: 2,
+        name: "Female",
+        value: "female"
+      }
+    ] 
+
     return (
       <div className={classNames(stepClasses)} step={step}>
-        Step Two
+        <Section>
+          <SectionHeader sectionTitle="Patient Information" />
+          <TextInput inputName="Patient First Name" inputRef={c => this.firstName = c} />
+          <TextInput inputName="Last Name" inputRef={c => this.lastName = c} />
+          <NumberInput inputName="Age Yrs" inputRef={c => this.ageYrs = c} />
+          <Dropdown inputName="Sex" inputRef={c => this.ageYrs = c} options={sexOptions} />
+          <NextStep {...this.props} />
+        </Section>
       </div>
     );
   }
